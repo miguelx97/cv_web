e05cb94c01e7c4df6fbeda2dcf8d9dcb424c1b02
MiguelcMartín <miguelmartin97@hotmail.com>
2024-08-14 13:19:45 +0700

    added projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './sections/header/header.component';
 import { SkillsComponent } from './sections/skills/skills.component';
 import { TitleComponent } from './components/title/title.component';
+import { ProjectsComponent } from './sections/projects/projects.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SkillsComponent],
+  imports: [RouterOutlet, HeaderComponent, SkillsComponent, ProjectsComponent],
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
---
+++ src/app/sections/projects/projects.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { GlobalModules } from '../../global_modules';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [GlobalModules],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent {
+
+}
